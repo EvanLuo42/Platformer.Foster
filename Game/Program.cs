cd8259893e7b08f617f11888dfa95dfd0095d09f
Evan Luo <knacks.insects_0o@icloud.com>
2024-02-06 13:41:34 +0800

    Initial commit

---
+++ Game/Program.cs
@@ -0,0 +1,17 @@
+﻿using Foster.Framework;
+
+namespace Platformer.Game;
+
+class Program
+{
+    public static void Main()
+    {
+        App.Register<Manager>();
+        App.Run("Platformer", 1280, 720);
+    }
+}
+
+public class Manager : Module
+{
+
+}
